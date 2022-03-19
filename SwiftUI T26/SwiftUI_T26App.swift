0e5db03822fa7e85a14c061dc8b74459e76e33c0
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2022-03-19 17:57:53 +0700

    Initial commit

---
+++ SwiftUI T26/SwiftUI_T26App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_T26App.swift
+//  SwiftUI T26
+//
+//  Created by M Fadli Zein on 19/03/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_T26App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
